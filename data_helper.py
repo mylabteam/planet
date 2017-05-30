3cbbbe2a63c24828b6d57d350b8621a93bcaf2de
Sarun Gulyanon <sgulyano@iupui.edu>
2017-05-29 22:43:28 -0400

    Modify code from Godard for Python2.7.

---
+++ data_helper.py
@@ -0,0 +1,221 @@
+import os
+import sys
+import gc
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from PIL import Image
+from itertools import chain
+from multiprocessing import cpu_count
+from concurrent.futures import ThreadPoolExecutor
+
+
+def get_jpeg_data_files_paths(data_root_folder=os.path.abspath("./")):
+    """
+    Returns the input file folders path
+    
+    :return: list of strings
+        The input file paths as list [train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file]
+    """
+    train_jpeg_dir = os.path.join(data_root_folder, 'train-jpg')
+    test_jpeg_dir = os.path.join(data_root_folder, 'test-jpg')
+    test_jpeg_additional = os.path.join(data_root_folder, 'test-jpg-additional')
+    train_csv_file = os.path.join(data_root_folder, 'train_v2.csv')
+    return [train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file]
+
+
+def _train_transform_to_matrices(*args):
+    """
+    
+    :param args: list of arguments
+        file_path: string
+            The path of the image
+        tags: list of strings
+            The associated tags
+        labels_map: dict {int: string}
+            The map between the image label and their id 
+        img_resize: tuple (int, int)
+            The resize size of the original image given by the file_path argument
+    :return: img_array, targets
+        img_array: Numpy array
+            The image from the file_path as a numpy array resized with img_resize
+        targets: Numpy array
+            A 17 length vector
+    """
+    # Unpack the *args
+    file_path, tags, labels_map, img_resize = list(args[0])
+    img = Image.open(file_path)
+    img.thumbnail(img_resize)  # Resize the image
+
+    # Augment the image `img` here
+
+    # Convert to RGB and normalize
+    img_array = np.asarray(img.convert("RGB"), dtype=np.float32) / 255
+
+    targets = np.zeros(len(labels_map))
+    for t in tags.split(' '):
+        targets[labels_map[t]] = 1
+    return img_array, targets
+
+
+def _test_transform_to_matrices(*args):
+    """
+    :param args: list of arguments
+        test_set_folder: string
+            The path of the all the test images
+        file_name: string
+            The name of the test image
+        img_resize: tuple (int, int)
+            The resize size of the original image given by the file_path argument
+        :return: img_array, file_name
+            img_array: Numpy array
+                The image from the file_path as a numpy array resized with img_resize
+            file_name: string
+                The name of the test image
+        """
+    test_set_folder, file_name, img_resize = list(args[0])
+    img = Image.open('{}/{}'.format(test_set_folder, file_name))
+    img.thumbnail(img_resize)
+
+    # Augment the image `img` here
+
+    # Convert to RGB and normalize
+    img_array = np.array(img.convert("RGB"), dtype=np.float32) / 255
+    return img_array, file_name
+
+
+def _get_train_matrices(train_set_folder, train_csv_file, img_resize, process_count):
+    """
+    
+    :param train_set_folder: string
+        The path of the all the train images
+    :param train_csv_file: string
+        The path of the csv file labels
+    :param img_resize: tuple (int, int)
+        The resize size of the original image given by the file_path argument
+    :param process_count: int
+        The number of threads you want to spawn to transform raw images to numpy
+        matrices
+    :return: x_train, y_train, labels_map
+        x_train: list of float matrices
+            The list of all the images stored as numpy matrices
+        y_train: list of list of int
+            A list containing vectors of 17 length long ints
+        labels_map: dict {string: int}
+            Inverted mapping of labels/id
+    """
+    labels_df = pd.read_csv(train_csv_file)
+    labels = sorted(set(chain.from_iterable([tags.split(" ") for tags in labels_df['tags'].values])))
+    labels_map = {l: i for i, l in enumerate(labels)}
+
+    files_path = []
+    tags_list = []
+    for file_name, tags in labels_df.values:
+        files_path.append('{}/{}.jpg'.format(train_set_folder, file_name))
+        tags_list.append(tags)
+
+    x_train = []
+    y_train = []
+    # Multiprocess transformation, the map() function take a function as a 1st argument
+    # and the argument to pass to it as the 2nd argument. These arguments are processed
+    # asynchronously on threads defined by process_count and their results are stored in
+    # the x_train and y_train lists
+    with ThreadPoolExecutor(process_count) as pool:
+        for img_array, targets in tqdm(pool.map(_train_transform_to_matrices,
+                                                [(file_path, tag, labels_map, img_resize)
+                                                 for file_path, tag in zip(files_path, tags_list)]),
+                                       total=len(files_path)):
+            x_train.append(img_array)
+            y_train.append(targets)
+    return [x_train, y_train, {v: k for k, v in labels_map.items()}]
+
+
+def _get_test_matrices(test_set_folder, img_resize, process_count):
+    x_test = []
+    x_test_filename = []
+    files_name = os.listdir(test_set_folder)
+
+    # Multiprocess transformation, the map() function take a function as a 1st argument
+    # and the argument to pass to it as the 2nd argument. These arguments are processed
+    # asynchronously on threads defined by process_count and their results are stored in
+    # the x_test and x_test_filename lists
+    with ThreadPoolExecutor(process_count) as pool:
+        for img_array, file_name in tqdm(pool.map(_test_transform_to_matrices,
+                                                  [(test_set_folder, file_name, img_resize)
+                                                   for file_name in files_name]),
+                                         total=len(files_name)):
+            x_test.append(img_array)
+            x_test_filename.append(file_name)
+    return [x_test, x_test_filename]
+
+
+def preprocess_train_data(train_set_folder, train_csv_file, img_resize=(32, 32), process_count=cpu_count()):
+    """
+    Transform the train images to ready to use data for the CNN 
+    :param train_set_folder: the folder containing the images for training
+    :param train_csv_file: the file containing the labels of the training images
+    :param img_resize: the standard size you want to have on images when transformed to matrices
+    :param process_count: the number of process you want to use to preprocess the data.
+        If you run into issues, lower this number. Its default value is equal to the number of core of your CPU
+    :return: The images matrices and labels as [x_train, y_train, labels_map]
+        x_train: The X train values as a numpy array
+        y_train: The Y train values as a numpy array
+        labels_map: The mapping between the tags labels and their indices
+    """
+    x_train, y_train, labels_map = _get_train_matrices(train_set_folder, train_csv_file, img_resize, process_count)
+    ret = [np.array(x_train), np.array(y_train, dtype=np.uint8), labels_map]
+    print("Done. Size consumed by arrays {} mb".format((ret[0].nbytes + ret[1].nbytes) / 1024 / 1024))
+    return ret
+
+
+def preprocess_test_data(test_set_folder, img_resize=(32, 32), process_count=cpu_count()):
+    """
+    Transform the images to ready to use data for the CNN
+    :param test_set_folder: the folder containing the images for testing
+    :param img_resize: the standard size you want to have on images when transformed to matrices
+    :param process_count: the number of process you want to use to preprocess the data.
+        If you run into issues, lower this number. Its default value is equal to the number of core of your CPU
+    :return: The images matrices and labels as [x_test, x_test_filename]
+        x_test: The X test values as a numpy array
+        x_test_filename: The files name of each test images in the same order as the x_test arrays
+    """
+    x_test, x_test_filename = _get_test_matrices(test_set_folder, img_resize, process_count)
+    ret = [np.array(x_test), x_test_filename]
+    print("Done. Size consumed by arrays {} mb".format(ret[0].nbytes / 1024 / 1024))
+    return ret
+
+
+def preprocess_data(train_set_folder, test_set_folder,
+                    test_set_additional, train_csv_file, img_resize=(32, 32), process_count=cpu_count()):
+    """
+    Transform the all the images to ready to use data for the CNN
+    :param train_set_folder: the folder containing the images for training
+    :param test_set_folder: the folder containing the images for testing
+    :param test_set_additional: the folder containing the images for additional testing (updated on 05/05/2017) 
+            https://www.kaggle.com/c/planet-understanding-the-amazon-from-space/discussion/32157
+    :param train_csv_file: the file containing the labels of the training images
+    :param img_resize: the standard size you want to have on images when transformed to matrices
+    :param process_count: the number of process you want to use to preprocess the data.
+        If you run into issues, lower this number. Its default value is equal to the number of core of your CPU
+    :return: The images matrices and labels as [x_train, x_test, y_train, labels_map, x_test_filename]
+        x_train: The X train values as a numpy array
+        x_test: The X test values as a numpy array
+        y_train: The Y train values as a numpy array
+        labels_map: The mapping between the tags labels and their indices
+        x_test_filename: The files name of each test images in the same order as the x_test arrays
+    """
+    print("Transforming and augmenting train data to matrices. Using {} threads...".format(process_count))
+    sys.stdout.flush()
+    x_train, y_train, labels_map = _get_train_matrices(train_set_folder, train_csv_file, img_resize, process_count)
+    print("Transforming and augmenting test data to matrices. Using {} threads...".format(process_count))
+    sys.stdout.flush()
+    x_test, x_test_filename = _get_test_matrices(test_set_folder, img_resize, process_count)
+    print("Transforming and augmenting additional test data to matrices. Using {} threads...".format(process_count))
+    sys.stdout.flush()
+    x_test_add, x_test_filename_add = _get_test_matrices(test_set_additional, img_resize, process_count)
+    x_test = np.vstack((x_test, x_test_add))
+    x_test_filename = np.hstack((x_test_filename, x_test_filename_add))
+    ret = [np.array(x_train), np.array(x_test), np.array(y_train, dtype=np.uint8), labels_map, x_test_filename]
+    gc.collect()
+    print("Done. Size consumed by arrays {} mb".format((ret[0].nbytes + ret[1].nbytes + ret[2].nbytes) /1024/1024))
+    return ret
---
+++ my_amazon_forest.py
@@ -0,0 +1,292 @@
+# -*- coding: utf-8 -*-
+# # Planet: Understanding the Amazon deforestation from Space challenge
+# Special thanks to the kernel contributors of this challenge (especially @anokas and @Kaggoo) who helped me find a starting point for this notebook.
+# 
+# The whole code including the `data_helper.py` and `keras_helper.py` files are available on github [here](https://github.com/EKami/planet-amazon-deforestation) and the notebook can be found on the same github [here](https://github.com/EKami/planet-amazon-deforestation/blob/master/notebooks/amazon_forest_notebook.ipynb)
+# 
+# **If you found this notebook useful some upvotes would be greatly appreciated! :) **
+# Start by adding the helper files to the python path
+
+
+
+
+# Import required modules
+import os
+import gc
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import data_helper
+from keras_helper import AmazonKerasClassifier
+
+#%matplotlib inline
+#%config InlineBackend.figure_format = 'retina'
+
+# Print tensorflow version for reuse (the Keras module is used directly from the tensorflow framework)
+
+tf.__version__
+
+## Inspect image labels
+# Visualize what the training set looks like
+
+train_jpeg_dir, test_jpeg_dir, test_jpeg_additional, train_csv_file = data_helper.get_jpeg_data_files_paths()
+labels_df = pd.read_csv(train_csv_file)
+labels_df.head()
+
+# Each image can be tagged with multiple tags, lets list all uniques tags
+
+# Print all unique tags
+from itertools import chain
+labels_list = list(chain.from_iterable([tags.split(" ") for tags in labels_df['tags'].values]))
+labels_set = set(labels_list)
+print("There is {} unique labels including {}".format(len(labels_set), labels_set))
+
+
+# ### Repartition of each labels
+# Histogram of label instances
+labels_s = pd.Series(labels_list).value_counts() # To sort them by count
+fig, ax = plt.subplots(figsize=(16, 8))
+sns.barplot(x=labels_s, y=labels_s.index, orient='h')
+
+
+# ## Images
+# Visualize some chip images to know what we are dealing with.
+# Lets vizualise 1 chip for the 17 images to get a sense of their differences.
+
+# <codecell>
+
+images_title = [labels_df[labels_df['tags'].str.contains(label)].iloc[i]['image_name'] + '.jpg' 
+                for i, label in enumerate(labels_set)]
+
+plt.rc('axes', grid=False)
+_, axs = plt.subplots(5, 4, sharex='col', sharey='row', figsize=(15, 20))
+axs = axs.ravel()
+
+for i, (image_name, label) in enumerate(zip(images_title, labels_set)):
+    img = mpimg.imread(train_jpeg_dir + '/' + image_name)
+    axs[i].imshow(img)
+    axs[i].set_title('{} - {}'.format(image_name, label))
+
+# <markdowncell>
+
+# # Image Resize
+# Define the dimensions of the image data trained by the network. Due to memory constraints we can't load in the full size 256x256 jpg images. Recommended resized images could be 32x32, 64x64, or 128x128.
+
+# <codecell>
+
+img_resize = (64, 64) # The resize size of each image
+
+# <markdowncell>
+
+# # Data preprocessing
+# Preprocess the data in order to fit it into the Keras model.
+# 
+# Due to the hudge amount of memory the resulting matrices will take, the preprocessing will be splitted into several steps:
+#     - Preprocess training data (images and labels) and train the neural net with it
+#     - Delete the training data and call the gc to free up memory
+#     - Preprocess the first testing set
+#     - Predict the first testing set labels
+#     - Delete the first testing set
+#     - Preprocess the second testing set
+#     - Predict the second testing set labels and append them to the first testing set
+#     - Delete the second testing set
+
+# <codecell>
+
+x_train, y_train, y_map = data_helper.preprocess_train_data(train_jpeg_dir, train_csv_file, img_resize)
+# Free up all available memory space after this heavy operation
+gc.collect();
+
+# <codecell>
+
+print("x_train shape: {}".format(x_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+y_map
+
+# <markdowncell>
+
+# ## Create a checkpoint
+# 
+# Creating a checkpoint saves the best model weights across all epochs in the training process. This ensures that we will always use only the best weights when making our predictions on the test set rather than using the default which takes the final score from the last epoch. 
+
+# <codecell>
+
+from tensorflow.contrib.keras.api.keras.callbacks import ModelCheckpoint
+
+filepath="weights.best.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True)
+
+# <markdowncell>
+
+# ## Choose Hyperparameters
+# 
+# Choose your hyperparameters below for training. 
+
+# <codecell>
+
+validation_split_size = 0.2
+batch_size = 128
+
+# <markdowncell>
+
+# ## Define and Train model
+# 
+# Here we define the model and begin training. 
+# 
+# Note that we have created a learning rate annealing schedule with a series of learning rates as defined in the array `learn_rates` and corresponding number of epochs for each `epochs_arr`. Feel free to change these values if you like or just use the defaults. 
+
+# <codecell>
+
+classifier = AmazonKerasClassifier()
+classifier.add_conv_layer(img_resize)
+classifier.add_flatten_layer()
+classifier.add_ann_layer(len(y_map))
+
+train_losses, val_losses = [], []
+epochs_arr = [10, 5, 5]
+learn_rates = [0.001, 0.0001, 0.00001]
+for learn_rate, epochs in zip(learn_rates, epochs_arr):
+    tmp_train_losses, tmp_val_losses, fbeta_score = classifier.train_model(x_train, y_train, learn_rate, epochs, 
+                                                                           batch_size, validation_split_size=validation_split_size, 
+                                                                           train_callbacks=[checkpoint])
+    train_losses += tmp_train_losses
+    val_losses += tmp_val_losses
+
+# <markdowncell>
+
+# ## Load Best Weights
+
+# <markdowncell>
+
+# Here you should load back in the best weights that were automatically saved by ModelCheckpoint during training
+
+# <codecell>
+
+classifier.load_weights("weights.best.hdf5")
+print("Weights loaded")
+
+# <markdowncell>
+
+# ## Monitor the results
+
+# <markdowncell>
+
+# Check that we do not overfit by plotting the losses of the train and validation sets
+
+# <codecell>
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(val_losses, label='Validation loss')
+plt.legend();
+
+# <markdowncell>
+
+# Look at our fbeta_score
+
+# <codecell>
+
+fbeta_score
+
+# <markdowncell>
+
+# Before launching our predictions lets preprocess the test data and delete the old training data matrices
+
+# <codecell>
+
+del x_train, y_train
+gc.collect()
+
+x_test, x_test_filename = data_helper.preprocess_test_data(test_jpeg_dir, img_resize)
+# Predict the labels of our x_test images
+predictions = classifier.predict(x_test)
+
+# <markdowncell>
+
+# Now lets launch the predictions on the additionnal dataset (updated on 05/05/2017 on Kaggle)
+
+# <codecell>
+
+del x_test
+gc.collect()
+
+x_test, x_test_filename_additional = data_helper.preprocess_test_data(test_jpeg_additional, img_resize)
+new_predictions = classifier.predict(x_test)
+
+del x_test
+gc.collect()
+predictions = np.vstack((predictions, new_predictions))
+x_test_filename = np.hstack((x_test_filename, x_test_filename_additional))
+print("Predictions shape: {}\nFiles name shape: {}\n1st predictions entry:\n{}".format(predictions.shape, 
+                                                                              x_test_filename.shape,
+                                                                              predictions[0]))
+
+# <markdowncell>
+
+# Before mapping our predictions to their appropriate labels we need to figure out what threshold to take for each class.
+# 
+# To do so we will take the median value of each classes.
+
+# <codecell>
+
+# For now we'll just put all thresholds to 0.2 
+thresholds = [0.2] * len(labels_set)
+
+# TODO complete
+tags_pred = np.array(predictions).T
+_, axs = plt.subplots(5, 4, figsize=(15, 20))
+axs = axs.ravel()
+
+for i, tag_vals in enumerate(tags_pred):
+    sns.boxplot(tag_vals, orient='v', palette='Set2', ax=axs[i]).set_title(y_map[i])
+
+# <markdowncell>
+
+# Now lets map our predictions to their tags and use the thresholds we just retrieved
+
+# <codecell>
+
+predicted_labels = classifier.map_predictions(predictions, y_map, thresholds)
+
+# <markdowncell>
+
+# Finally lets assemble and visualize our prediction for the test dataset
+
+# <codecell>
+
+tags_list = [None] * len(predicted_labels)
+for i, tags in enumerate(predicted_labels):
+    tags_list[i] = ' '.join(map(str, tags))
+
+final_data = [[filename.split(".")[0], tags] for filename, tags in zip(x_test_filename, tags_list)]
+
+# <codecell>
+
+final_df = pd.DataFrame(final_data, columns=['image_name', 'tags'])
+final_df.head()
+
+# <codecell>
+
+tags_s = pd.Series(list(chain.from_iterable(predicted_labels))).value_counts()
+fig, ax = plt.subplots(figsize=(16, 8))
+sns.barplot(x=tags_s, y=tags_s.index, orient='h');
+
+# <markdowncell>
+
+# If there is a lot of `primary` and `clear` tags, this final dataset may be legit...
+
+# <markdowncell>
+
+# And save it to a submission file
+
+# <codecell>
+
+final_df.to_csv('submission_file.csv', index=False)
+classifier.close()
+
+# <markdowncell>
+
+# That's it, we're done!
